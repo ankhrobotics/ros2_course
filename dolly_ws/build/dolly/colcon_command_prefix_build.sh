2d6838fc11fc1e7b83009bb97f11e8e8422e085b
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2020-07-15 22:56:35 +0200

    removing build

--- dolly_ws/build/dolly/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/hema/ankhrobotics.github.io/Lectures/ros2_course/dolly_ws/install/dolly_follow/share/dolly_follow/package.sh"
-. "/home/hema/ankhrobotics.github.io/Lectures/ros2_course/dolly_ws/install/dolly_gazebo/share/dolly_gazebo/package.sh"
-. "/home/hema/ankhrobotics.github.io/Lectures/ros2_course/dolly_ws/install/dolly_ignition/share/dolly_ignition/package.sh"
